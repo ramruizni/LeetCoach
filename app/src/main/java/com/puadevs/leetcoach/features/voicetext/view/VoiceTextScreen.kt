b22b530d81528424dbe32e5caa5f7a7d71acc918
daniospina <ingedanielospina9@gmail.com>
2025-10-06 16:02:00 -0500

    feat: first simple implementation screen voivetext

--- app/src/main/java/com/puadevs/leetcoach/features/voicetext/view/VoiceTextScreen.kt
+++ app/src/main/java/com/puadevs/leetcoach/features/voicetext/view/VoiceTextScreen.kt
@@ -1,25 +1,94 @@
 package com.puadevs.leetcoach.features.voicetext.view
 
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.puadevs.leetcoach.features.voicetext.viewmodel.VoiceTextViewModel
+import java.io.File
 
 @Composable
 fun VoiceTextScreen(
     viewModel: VoiceTextViewModel = viewModel()
 ) {
     Scaffold { innerPadding ->
+
+        val context = LocalContext.current
+
+        val audioState by viewModel.audioState.collectAsStateWithLifecycle()
+
+        val permissionLauncher = rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { granted ->
+            viewModel.setPermissionGranted(granted)
+            if (!granted) {
+                viewModel.setTranscription("Audio recording permission denied.")
+            }
+        }
+
+        LaunchedEffect(Unit) {
+            permissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+        }
+
+        val audioFile = remember {
+            File(context.externalCacheDir, "recorded_audio.m4a")
+        }.toString()
+
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-            ) { }
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+            Row() {
+                Button(
+                    onClick = {
+                        if (!audioState.permissionGranted) {
+                            permissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+                            return@Button
+                        }
+                        viewModel.setStartButtonEnabled(false)
+                        viewModel.setStopButtonEnabled(true)
+                        viewModel.start(audioUri = audioFile)
+                    },
+                    enabled = audioState.startButtonEnabled
+                ) {
+                    Text(
+                        text = "Start"
+                    )
+                }
+                Button(
+                    onClick = {
+                        viewModel.setStopButtonEnabled(false)
+                        viewModel.setStartButtonEnabled(true)
+                        viewModel.stop(audioUri = audioFile)
+                        viewModel.transcriptAudio(audioUri = audioFile)
+                    },
+                    enabled = audioState.stopButtonEnabled
+                ) {
+                    Text(
+                        text = "Stop"
+                    )
+                }
+            }
+            Text(
+                text = audioState.transcription
+            )
+        }
     }
 }
--- app/src/main/java/com/puadevs/leetcoach/features/voicetext/viewmodel/VoiceTextViewModel.kt
+++ app/src/main/java/com/puadevs/leetcoach/features/voicetext/viewmodel/VoiceTextViewModel.kt
@@ -1,16 +1,77 @@
 package com.puadevs.leetcoach.features.voicetext.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.puadevs.leetcoach.voicetext.domain.usecases.RetrieveVoiceTextFrom
 import com.puadevs.leetcoach.voicetext.domain.usecases.StartRecording
 import com.puadevs.leetcoach.voicetext.domain.usecases.StopRecording
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
+data class AudioState(
+    val permissionGranted: Boolean = false,
+    val transcription: String = "",
+    val startButtonEnabled: Boolean = true,
+    val stopButtonEnabled: Boolean = false
+        )
 @HiltViewModel
 class VoiceTextViewModel @Inject constructor(
     private val retrieveVoiceTextFrom: RetrieveVoiceTextFrom,
     private val startRecording: StartRecording,
-    private val stopRecording: StopRecording
+    private val stopRecording: StopRecording,
+//    application: Application
 ) : ViewModel() {
+
+//    private val audioFile: String = File(application.externalCacheDir, "recorded_audio.m4a").toURI().toString()
+
+    private val _audioState = MutableStateFlow(AudioState())
+    val audioState = _audioState.asStateFlow()
+
+
+    fun setPermissionGranted(permissionGranted: Boolean) {
+        _audioState.update { it.copy(permissionGranted = permissionGranted) }
+    }
+    fun setTranscription(transcription: String) {
+        _audioState.update { it.copy(transcription = transcription) }
+    }
+
+    fun setStartButtonEnabled(startButtonEnabled: Boolean) {
+        _audioState.update { it.copy(startButtonEnabled = startButtonEnabled) }
+    }
+
+    fun setStopButtonEnabled(stopButtonEnabled: Boolean) {
+        _audioState.update { it.copy(stopButtonEnabled = stopButtonEnabled) }
+    }
+
+    fun start(audioUri: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            startRecording(audioUri = audioUri)
+        }
+    }
+
+    fun stop(audioUri: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _audioState.update { it.copy(transcription = "Transcribing") }
+            stopRecording(audioUri = audioUri)
+        }
+    }
+
+    fun transcriptAudio(audioUri: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val transcript = retrieveVoiceTextFrom(audioUri = audioUri)
+            if (transcript != null) {
+                _audioState.update { it.copy(transcription = transcript) }
+            } else {
+                _audioState.update { it.copy(transcription = "It was not possible to obtain the transcript\n") }
+            }
+        }
+    }
+
 }
